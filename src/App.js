0617194f4c2db2d51e309292626bcdd0efde2f56
spitfire182 <piotr.podg@gmail.com>
2020-04-23 23:54:58 +0100

    Adding Styled Components

--- src/App.js
+++ src/App.js
@@ -44,11 +44,16 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
       cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
+      },
     };
 
     let persons = null;
@@ -69,13 +74,27 @@ class App extends Component {
           })}
         </div>
       );
+
+      // style.backgroundColor = "red";
+      // style[":hover"] = {
+      //   backgroundColor: "salmon",
+      //   color: "black",
+      // };
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push("red");
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
     }
 
     return (
       <div className="App">
         <h1>React App</h1>
-        <p>It's working!</p>
-        <button style={style} onClick={() => this.togglePersonsHandler()}>
+        <p className={classes.join(" ")}>It's working!</p>
+        <button onClick={() => this.togglePersonsHandler()}>
           Toggle Persons
         </button>
         {persons}
