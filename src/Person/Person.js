3c9dfe8c9571b3b55798940d5447debfe77aea5b
spitfire182 <piotr.podg@gmail.com>
2020-04-24 22:25:06 +0100

    Adding Error Boundary to handle errors

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-import classes from"./Person.css";
+import classes from "./Person.css";
 
 const person = (props) => {
+  const rnd = Math.random();
+
+  if (rnd > 0.7) {
+    throw new Error("Something went wrong");
+  }
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>
